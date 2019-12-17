13b526aa07006d6e8d1a6a654b5265015dd55590
Krungthai Sumpurng <sosaom1@gmail.com>
2019-12-17 16:25:42 +0700

    Aomcommit

---
+++ 61030001/week8/week8/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+
+namespace ArrayListDemo
+{
+    class Program
+    {
+
+
+        static void Main(String[] args)
+        {
+            Hashtable name = new Hashtable();
+
+            name.Add("61030001", "krungthai");
+            name.Add("61030017", "napakorn");
+            name.Add("61030007", "komkrit");
+            name.Add("61030057", "sakulrat");
+            name.Add("61030009", "Janejira");
+            name.Add("61030040", "peerapon");
+            name.Add("61030045", "maethapon");
+            name.Add("61030021", "natthapat");
+
+            foreach (DictionaryEntry pp in name)
+            {
+                Console.WriteLine(pp.Key + "=" + pp.Value);
+            }
+            Console.WriteLine("*******ID Student Computer Class*********");
+            Console.Write("Press key ID=");
+            Console.WriteLine("" + name.ContainsKey(Console.ReadLine()));
+
+
+
+
+        }
+    }
+}
